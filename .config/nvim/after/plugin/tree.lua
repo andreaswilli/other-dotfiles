2eeced79392795f9c3491d2247363f88c5d130ee
Andreas Willi <andreas.willi@axa.ch>
2023-03-01 13:49:45 +0100

    Close tree when opening a file

--- .config/nvim/after/plugin/tree.lua
+++ .config/nvim/after/plugin/tree.lua
@@ -2,6 +2,11 @@ require("nvim-tree").setup({
   update_focused_file = {
     enable = true,
   },
+  actions = {
+    open_file = {
+      quit_on_open = true,
+    },
+  },
 })
 
 local tree = require("nvim-tree.api").tree
