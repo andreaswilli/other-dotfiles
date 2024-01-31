dab26d8f3a454a2d174173dd22be22793c56b980
Andreas Willi <andiwilli8460@gmail.com>
2024-01-31 20:16:42 +0100

    Add git key maps

--- .config/nvim/after/plugin/gitsigns.lua
+++ .config/nvim/after/plugin/gitsigns.lua
@@ -1,1 +1,12 @@
 require('gitsigns').setup()
+
+-- gitsigns
+vim.keymap.set('n', '<leader>gh', ':Gitsigns preview_hunk<CR>', { silent = true })
+vim.keymap.set('n', '<leader>gn', ':Gitsigns next_hunk<CR>', { silent = true })
+vim.keymap.set('n', '<leader>gp', ':Gitsigns prev_hunk<CR>', { silent = true })
+vim.keymap.set('n', '<leader>gr', ':Gitsigns reset_hunk<CR>', { silent = true })
+vim.keymap.set('n', '<leader>gl', ':Gitsigns toggle_current_line_blame<CR>', { silent = true })
+
+-- fugitive
+vim.keymap.set('n', '<leader>gb', ':Git blame<CR>', { silent = true })
+vim.keymap.set('n', '<leader>gs', ':tab Git<CR>', { silent = true })
