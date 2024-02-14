bef022390664ec88503a1c7eafd10745e166065f
Andreas Willi <andreas.willi@axa.ch>
2024-02-14 11:02:05 +0100

    Make git blame dates relative

--- .config/nvim/after/plugin/gitsigns.lua
+++ .config/nvim/after/plugin/gitsigns.lua
@@ -8,5 +8,5 @@ vim.keymap.set('n', '<leader>gr', ':Gitsigns reset_hunk<CR>', { silent = true })
 vim.keymap.set('n', '<leader>gl', ':Gitsigns toggle_current_line_blame<CR>', { silent = true })
 
 -- fugitive
-vim.keymap.set('n', '<leader>gb', ':Git blame<CR>', { silent = true })
+vim.keymap.set('n', '<leader>gb', ':Git blame --date=relative<CR>', { silent = true })
 vim.keymap.set('n', '<leader>gs', ':tab Git<CR>', { silent = true })
