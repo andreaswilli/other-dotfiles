e0ce04044c962c0377c0502f9256c1e9a4df6018
Andreas Willi <andiwilli8460@gmail.com>
2024-03-16 18:53:55 +0100

    Add copy/paste key maps and remove duplication

--- .config/nvim/lua/nd/map.lua
+++ .config/nvim/lua/nd/map.lua
@@ -10,15 +10,16 @@ vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
 
-vim.keymap.set('n', '<leader>y', '"+y')
-vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set({'n', 'v'}, '<leader>y', '"+y')
 vim.keymap.set('n', '<leader>Y', '"+Y')
 
 vim.keymap.set('x', 'p', '"_dP')
-vim.keymap.set('n', '<leader>d', '"_d')
-vim.keymap.set('v', '<leader>d', '"_d')
-vim.keymap.set('n', '<leader>c', '"_c')
-vim.keymap.set('v', '<leader>c', '"_c')
+vim.keymap.set({'n', 'v'}, '<leader>d', '"_d')
+vim.keymap.set({'n', 'v'}, '<leader>c', '"_c')
+
+vim.keymap.set('v', '<C-c>', '"+y')
+vim.keymap.set({'n', 'v'}, '<C-v>', '"+p')
+vim.keymap.set('i', '<C-v>', '<esc>"+p`]a')
 
 vim.cmd('command -nargs=* H tab h <args>')
 vim.cmd('command -nargs=* Help tab h <args>')
