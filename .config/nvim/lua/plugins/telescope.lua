3a465283c29bb6ffdcc6f15e7322b738d36f96f1
Andreas Willi <andiwilli8460@gmail.com>
2024-12-25 22:50:40 +0100

    Add telescope multigrep

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,7 +2,10 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+    },
 
     opts = function()
       local actions = require('telescope.actions')
@@ -35,6 +38,7 @@ return {
           ['ui-select'] = {
             require("telescope.themes").get_cursor {}
           },
+          fzf = {},
         },
       }
     end,
@@ -42,11 +46,13 @@ return {
     config = function(_, opts)
       require('telescope').setup(opts)
       require('telescope').load_extension('ui-select')
+      require('telescope').load_extension('fzf')
+      require('config.telescope.multigrep').setup()
     end,
 
     keys = {
       { '<leader>f', '<cmd>Telescope find_files hidden=true<cr>' },
-      { '<leader>s', '<cmd>Telescope live_grep<cr>' },
+      -- { '<leader>s', '<cmd>Telescope live_grep<cr>' }, -- replaced by multigrep
       { '<leader>S', '<cmd>Telescope grep_string<cr>' },
       { '<leader>b', '<cmd>Telescope buffers<cr>' },
       { '<leader>H', '<cmd>Telescope help_tags<cr>' },
