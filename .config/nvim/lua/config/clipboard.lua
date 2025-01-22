1cc8ddbe95ba703de90bb3a87189c36ba6eb88fd
Andreas Willi <andreas.willi@axa.ch>
2025-01-22 18:37:07 +0100

    Add wsl clipboard config

---
+++ .config/nvim/lua/config/clipboard.lua
@@ -0,0 +1,18 @@
+if vim.fn.has("wsl") then
+
+  local clip = "/mnt/c/Windows/System32/clip.exe"
+  local powershell = "/mnt/c/eplatform/mytools/PowerShell-7.3.1-win-x64/pwsh.exe"
+
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = clip,
+      ["*"] = clip
+    },
+    paste = {
+      ["+"] = powershell .. ' -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = powershell .. ' -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))'
+    },
+    cache_enabled = 0
+  }
+end
