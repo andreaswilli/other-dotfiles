dfc11e04e31224560ecae7231c349e726d539119
Andreas Willi <andiwilli8460@gmail.com>
2025-07-08 20:02:50 +0200

    Add substitute mapping for visual mode

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -21,6 +21,7 @@ vim.keymap.set({ 'n', 'v' }, '<C-v>', '"+p`]')
 vim.keymap.set('i', '<C-v>', '<esc>"+p`]a')
 
 vim.keymap.set('n', '<leader>a', ':%s//g<Left><Left>')
+vim.keymap.set('v', '<leader>a', ':s//g<Left><Left>')
 
 vim.cmd('command -nargs=* H tab h <args>')
 vim.cmd('command -nargs=* Help tab h <args>')
