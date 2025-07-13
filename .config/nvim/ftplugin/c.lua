725fa9629f91660fe05d0b53fa8278a2ebb5ec1a
Andreas Willi <andiwilli8460@gmail.com>
2025-07-13 23:42:31 +0200

    Use vim api to run c and add error handling

--- .config/nvim/ftplugin/c.lua
+++ .config/nvim/ftplugin/c.lua
@@ -1,27 +1,27 @@
+local run = function(cmd)
+  local result = vim.system(cmd, { text = true }):wait()
+
+  if result.stdout ~= "" then
+    print("===== Stdout =====\n\n" .. result.stdout .. "\n")
+  end
+
+  if result.stderr ~= "" then
+    print("===== Stderr =====\n\n" .. result.stderr .. "\n")
+  end
+
+  return result.code
+end
+
 local compileAndRun = function()
   vim.cmd("silent write")
 
   local sourceName = vim.fn.expand("%")
   local binName = vim.fn.expand("%:r") .. ".bin"
-  local compile = "gcc " .. sourceName .. " -o " .. binName
-  local run = "./" .. binName
 
-  local compileOutFile = assert(io.popen(compile .. " 2>&1", "r"))
-  local compileOut = compileOutFile:read("*all")
-  compileOutFile:close()
-
-  if compileOut ~= "" then
-    print("Compiler Error:\n\n" .. compileOut .. "\n")
-    return
-  end
-
-  local runOutFile = assert(io.popen(run, "r"))
-  local runOut = runOutFile:read("*all")
-  runOutFile:close()
-
-  print("Output:\n\n" .. runOut .. "\n")
-
-  os.execute("rm -f " .. binName)
+  local code = run { "gcc", sourceName, "-o", binName }
+  if code ~= 0 then return end
+  run { binName }
+  run { "rm", "-f", binName }
 end
 
 vim.keymap.set('n', '<leader>o', compileAndRun, { buffer = true })
