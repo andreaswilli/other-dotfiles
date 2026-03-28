20d5f7b1c6da3003f9c24d2661893b8fe28be5b8
Andreas Willi <andiwilli8460@gmail.com>
2026-03-28 10:53:02 +0100

    Update treesitter, add quicker.nvim

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,9 @@
 return {
   'nvim-treesitter/nvim-treesitter',
 
-  build = function()
-    require('nvim-treesitter.install').update({ with_sync = true })()
-  end,
+  branch = 'main',
+
+  build = ':TSUpdate',
 
   lazy = false,
 
@@ -15,7 +15,7 @@ return {
       enable = true,
       disable = function(lang, buf)
         local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
         if ok and stats and stats.size > max_filesize then
           return true
         end
